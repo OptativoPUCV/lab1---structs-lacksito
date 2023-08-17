0ca4a335f8691db3c99a11558e995b1cfdac60c4
Replit user <>
2023-08-17 20:29:50 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -86,8 +86,7 @@ arreglos en un tercer arreglo también ordenado.
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
                        int result[]) 
 {
-  int arr3[];
-  for
+
 }
 
 /*
